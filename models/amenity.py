e0e8c562c5f7deafca3a9ae3d9e6436def7d7231
Abakpa Dominic <abakpad82@gmail.com>
2023-02-11 00:00:40 +0100

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" Amenity class """
-
-import uuid
-from datetime import datetime
-from models import storage
+"""
+Amenity class, a subclass of BaseModel
+"""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """ Amenity class """
+    """
+    A subclass of BaseModel class
+    Public class attribute:
+        name: (str)
+    """
     name = ""
